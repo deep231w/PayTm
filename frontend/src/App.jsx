e5eb1a0cef9075cf156878f16ecab971dfa9e97a
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-07-22 02:31:42 +0530

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,18 @@ import {
 import { Dashboard } from "./pages/Dashboard";
 
 import './index.css'
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return <>
   <BrowserRouter>
         <Routes>
-        
           <Route path="/dashboard" element={<Dashboard />} />
-         
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/send" element={<SendMoney/>}/>
         </Routes>
       </BrowserRouter>
       </>

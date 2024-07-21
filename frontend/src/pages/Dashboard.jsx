e55a6bdc4e337dcb756844f5bc35a97e5f0a0c45
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-07-21 21:56:02 +0530

    frontend updated

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,12 +1,16 @@
 import { Appbar } from "../components/Appbar";
-
-
+import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
 export const Dashboard = ()=>{
 
     return (
         <div>
             <Appbar  />
-            
+            <div className="m-8">
+                <Balance value={10000} />
+                {/* Your other components go here */}
+               <Users/>
+            </div>
         </div>
     );
 }
